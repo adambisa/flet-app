0b4b026594ec5e105a1575b5a52ed99f426c41bb
adambisa <117039930+adambisa@users.noreply.github.com>
2022-12-05 11:31:49 +0100

    ui
    app with ui

--- app.py
+++ app.py
@@ -3,47 +3,50 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = 'CALCULATORATOR 3000'
-    result = ft.Text(value=0)
+    txtcol=ft.colors.WHITE
+    numblack=ft.colors.BLACK54
+    result = ft.Text(value=0, color=txtcol)
+    
     column = ft.Column(
         [
-            ft.Row(controls=[result], alignment='end'),
+            ft.Row(controls=[result]),
             ft.Row(
                 controls=[
-                    ft.ElevatedButton(text="AC"),
-                    ft.ElevatedButton(text="+/-"),
-                    ft.ElevatedButton(text="%"),
-                    ft.ElevatedButton(text="/"),
+                    ft.ElevatedButton(text="AC", bgcolor=ft.colors.WHITE30,color=txtcol),
+                    ft.ElevatedButton(text="+/-", bgcolor=ft.colors.WHITE30,color=txtcol),
+                    ft.ElevatedButton(text="%", bgcolor=ft.colors.WHITE30,color=txtcol),
+                    ft.ElevatedButton(text="/",bgcolor=ft.colors.WHITE30,color=txtcol),
                 ]
             ),
             ft.Row(
                 controls=[
-                    ft.ElevatedButton(text="7"),
-                    ft.ElevatedButton(text="8"),
-                    ft.ElevatedButton(text="9"),
-                    ft.ElevatedButton(text="*"),
+                    ft.ElevatedButton(text="7",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="8",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="9",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="*", bgcolor=ft.colors.YELLOW_800,color=txtcol),
                 ]
             ),
             ft.Row(
                 controls=[
-                    ft.ElevatedButton(text="4"),
-                    ft.ElevatedButton(text="5"),
-                    ft.ElevatedButton(text="6"),
-                    ft.ElevatedButton(text="-"),
+                    ft.ElevatedButton(text="4",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="5",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="6",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="-", bgcolor=ft.colors.YELLOW_800,color=txtcol),
                 ]
             ),
             ft.Row(
                 controls=[
-                    ft.ElevatedButton(text="1"),
-                    ft.ElevatedButton(text="2"),
-                    ft.ElevatedButton(text="3"),
-                    ft.ElevatedButton(text="+"),
+                    ft.ElevatedButton(text="1",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="2",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="3",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="+", bgcolor=ft.colors.YELLOW_800,color=txtcol),
                 ]
             ),
             ft.Row(
                 controls=[
-                    ft.ElevatedButton(text="0"),
-                    ft.ElevatedButton(text="."),
-                    ft.ElevatedButton(text="="),
+                    ft.ElevatedButton(text="0",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text=".",color=txtcol,bgcolor=numblack),
+                    ft.ElevatedButton(text="=", bgcolor=ft.colors.YELLOW_800,color=txtcol),
                 ]
             ),
         ]
