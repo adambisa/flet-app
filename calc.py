58730a3114bd2b4b51e21a9587a314aeb491bef4
adambisa <117039930+adambisa@users.noreply.github.com>
2022-12-05 12:53:16 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -16,19 +16,25 @@ class CalcApp(UserControl):
 
     def build(self):
         textcol=colors.WHITE
-        self.textcol=textcol
-        result=Text(value=0,color=self.textcol, size=20)
+        btextcol=colors.BLACK
+        contrcol=colors.ORANGE_500
+        topcol=colors.WHITE30
+        result=Text(value=0,color=textcol, size=20)
         
         return Container(
-            width=300,
+            width=315,
             bgcolor=colors.BLACK,
             border_radius=border_radius.all(20),
             padding=20,
             content=Column(
                 [
-                    Row(controls=[ElevatedButton(text='AC'), ElevatedButton(text='+/-'), ElevatedButton(text='%'), ElevatedButton(text='/')])
+                    Row(controls=[result], alignment='end'),
+                    Row(controls=[ElevatedButton(text='AC', bgcolor=topcol,color=textcol), ElevatedButton(text='+/-',bgcolor=topcol,color=textcol), ElevatedButton(text='%',bgcolor=topcol,color=textcol), ElevatedButton(text='/', bgcolor=contrcol,color=textcol)]),
+                    Row(controls=[ElevatedButton(text='7',color=btextcol),ElevatedButton(text='8',color=btextcol),ElevatedButton(text='9',color=btextcol), ElevatedButton(text='*', bgcolor=contrcol,color=textcol)]),
+                    Row(controls=[ElevatedButton(text='4',color=btextcol),ElevatedButton(text='5',color=btextcol),ElevatedButton(text='6',color=btextcol),ElevatedButton(text='-', bgcolor=contrcol,color=textcol)]),
+                    Row(controls=[ElevatedButton(text='1',color=btextcol),ElevatedButton(text='2',color=btextcol),ElevatedButton(text='3',color=btextcol),ElevatedButton(text='+', bgcolor=contrcol,color=textcol)]),
+                    Row(controls=[ElevatedButton(text='0',color=btextcol,expand=2),ElevatedButton(text='.',color=btextcol), ElevatedButton(text='=',color=btextcol)])
                 ]
-                
             )
         )
 def main(page:Page):
